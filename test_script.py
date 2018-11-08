d7797ac6bfac0575c4d9dfb44be02102943b29c7
Andrew_Reed <andrew.c.reed88@gmail.com>
2018-11-08 16:18:19 -0500

    Now importing numpy and taking the square root of the value

--- test_script.py
+++ test_script.py
@@ -1,8 +1,9 @@
 # This is a test script for python shell
 import time
+import numpy as np
 x=0
 for i in range(1,11):
-	x = x+i**2
+	x = np.sqrt(x+i**2)
 	print('The new count is: ' + str(x))
 	time.sleep(1)
 
